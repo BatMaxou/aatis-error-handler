01d1e10cbc25e55c83f6d7f233a3584ad5a52c40
Maxime <maxime@maximebatista.lycee@gmail.com>
2024-01-24 21:09:24 +0100

    :construction: Add templates

---
+++ templates/components/tracesStepLine.php
@@ -0,0 +1,4 @@
+<div class="traceLine<?php echo ($step['line'] === $lineNumber) ? ' hightlighted' : '' ?>">
+    <p class="lineNumber"><?php echo $lineNumber ?></p>
+    <p class="lineContent"><?php echo $lineContent ?></p>
+</div>
--- templates/error.tpl.php
+++ templates/error.tpl.php
@@ -4,11 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title . $code . $level ?></title>
+    <title><?php echo $type . ' ' . ($code ?? $level) ?></title>
 </head>
 
 <body>
-    <?php echo $title, ': [', $code, '] [', $level, '] ', $message, ' - ', $file, ':', $line ?>
+    <div class="header">
+        <div class="errorDescription">
+            <h1><?php echo $type . ': ' . $message ?></h1>
+            <p class="subText"><?php echo 'in ' . $file . ' (line ' . $line . ')' ?></p>
+        </div>
+        <div class="errorInfos">
+            <p class="subText"><?php echo 'Code' . ($code ?? $level) ?>
+            <p class="subText"><?php echo 'Enum Category Error/Exception' ?>
+        </div>
+    </div>
+
+    <div class="content">
+        <?php foreach ($trace as $step) include __DIR__ . '/components/traceStep.tpl.php' ?>
+    </div>
 </body>
 
 </html>
